7df68ee624738f874149ada72bdcb5f4969288c5
KaterynaBog <skif090402@gmail.com>
2025-11-04 18:49:59 +0200

    Реалізовано адаптивний дизайн
    Використано медіа запит та налаштоване адаптивне меню навігації(бургер-форма).
    Помарка: не дороблено адаптивність до кінця

--- js/script.js
+++ js/script.js
@@ -21,5 +21,13 @@ document.addEventListener("DOMContentLoaded", () => {
       title.textContent = "Вхід";
     });
   }
-});
+  const burger = document.getElementById("burger");
+  const nav = document.querySelector("nav");
 
+  if (burger && nav) {
+    burger.addEventListener("click", () => {
+      burger.classList.toggle("active");
+      nav.classList.toggle("active");
+    });
+  }
+});
